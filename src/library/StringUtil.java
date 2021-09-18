93124a01e1160e03bd4b5f37ce89e905416d198f
irfandocker <irfan.gt11@gmail.com>
2021-09-18 12:33:52 -0400

    Update StringUtil.java

--- src/library/StringUtil.java
+++ src/library/StringUtil.java
@@ -5,35 +5,53 @@ public class StringUtil {
  /*
     returns the frequency of given char from the given string, as int
  */
+//	String str = "hello" char a = 'e'; 
+//	output 1
 	
 	
 	/*
     returns the unique characters for the given string
  */
-	
+//	input: aabbcc
+//	output: abc
+//	
 	
 	
 	/*
     reverses the given string object and returns it
  */
 	
+//	input:abc
+//	output:cba
+	
 	
 	 /*
     removes the duplicates from the string and returns it
     */
 	
+//	input: aabbcc
+//	output: abc
+	
+	
 	  /*
     returns the frequency of characters
      */
+//	String str = "hello" char a = 'e'; 
+//	output 1
+	
 	
 	 /*
     verify if the string palindrome
      */
 	
-//	civic
+//	civic 
 	
 	 /*
     takes a sentence and word, then returns the frequency of the word
      */
+	
+//	input: "Hello java" 
+//		looking for "Hello"
+//	output : 1 time
 	
 }
